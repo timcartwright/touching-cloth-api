49555b1120107b629ec0d5b77b2a6b6de0075116
timcartwright <tim@websitesbytim.com>
2017-12-03 23:55:24 +0000

    Add new player to leaderboard  on create

--- common/models/player.js
+++ common/models/player.js
@@ -2,4 +2,11 @@
 
 module.exports = function(Player) {
 
+    Player.observe('before save', function(ctx, next) {
+        Player.count((err, count) => {
+            ctx.instance.ladderRank = count + 1;
+        });
+        next();
+      });
+      
 };
